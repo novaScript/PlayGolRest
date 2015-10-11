24c0a1bdc22b1c020e94a65a1f89ed90c6364392
David Pineda <davidpineda@gmail.com>
2015-10-10 22:08:26 -0500

    Se agrega soporte para departamentos agrupados

--- PlayGolRest/controllers/dataResidenceCtrl.js
+++ PlayGolRest/controllers/dataResidenceCtrl.js
@@ -6,12 +6,15 @@ Descripcion: Control que maneja las consultas sobre el objeto datos de residenci
 */
 
 //File: controllers/dataResidenceCtrl.js
-var mongoose = require('mongoose');
-var DataResidence = mongoose.model('data_residence');
+var mongoose = require("mongoose");
+var DataResidence = mongoose.model("data_residence");
 
 //GET - Retorna todos los estados o departamentos de un pais
 exports.findAllStates = function (req, res) {
-    DataResidence.find({pais: req.params.pais}, function (err, states) {
+    DataResidence.aggregate([ 
+        { $match: { pais: req.params.pais } },
+        { $group: { _id: "$departamento" } }
+    ], function (err, states) {
         if (err) res.send(500, err.message);
         res.status(200).jsonp(states);
     });
