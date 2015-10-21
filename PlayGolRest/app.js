880369773b5f435ab56e6defac9b9958bc1ba329
David Pineda <davidpineda@gmail.com>
2015-10-20 23:11:08 -0500

    Se agrega la consulta de ciudades

--- PlayGolRest/app.js
+++ PlayGolRest/app.js
@@ -2,13 +2,13 @@
     app = express(),
     bodyParser = require("body-parser"),
     methodOverride = require("method-override"),
-    mongoose = require('mongoose');
+    mongoose = require("mongoose");
 
 // Connection to DB
 var url = "mongodb://localhost/PlayGol";
 mongoose.connect(url, function (err, res) {
     if (err) throw err;
-    console.log('Connected to Database');
+    console.log("Connected to Database");
 });
 
 // Middlewares
@@ -17,14 +17,14 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 
 // Import Models and controllers
-var userModel = require('./models/userModel')(app, mongoose);
-var userCtrl = require('./controllers/userCtrl');
-var dataResidenceModel = require('./models/dataResidenceModel')(app, mongoose);
-var dataResidenceCtrl = require('./controllers/dataResidenceCtrl');
+var userModel = require("./models/userModel")(app, mongoose);
+var userCtrl = require("./controllers/userCtrl");
+var dataResidenceModel = require("./models/dataResidenceModel")(app, mongoose);
+var dataResidenceCtrl = require("./controllers/dataResidenceCtrl");
 
 // Example Route
 var router = express.Router();
-router.get('/', function (req, res) {
+router.get("/", function (req, res) {
     res.send("Hello world!");
 });
 app.use(router);
@@ -32,23 +32,26 @@ app.use(router);
 // API routes
 var users = express.Router();
 
-users.route('/users')
+users.route("/users")
     .get(userCtrl.findAllUsers)
     .post(userCtrl.addUser);
 
-users.route('/users/:id')
+users.route("/users/:id")
     .get(userCtrl.findUserById)
     .put(userCtrl.updateUser)
     .delete(userCtrl.deleteUser);
 
-app.use('/api', users);
+app.use("/api", users);
 
 var dataResidence = express.Router();
 
-dataResidence.route('/data_residence/:pais')
+dataResidence.route("/data_residence/:pais")
     .get(dataResidenceCtrl.findAllStates);
 
-app.use('/api', dataResidence);
+dataResidence.route("/data_residence/:pais/:departamento")
+    .get(dataResidenceCtrl.findAllCitiesForStates);
+
+app.use("/api", dataResidence);
 
 // Start server
 app.listen(3000, function () { 
--- PlayGolRest/controllers/dataResidenceCtrl.js
+++ PlayGolRest/controllers/dataResidenceCtrl.js
@@ -10,7 +10,7 @@ var mongoose = require("mongoose");
 var DataResidence = mongoose.model("data_residence");
 
 //GET - Retorna todos los estados o departamentos de un pais
-exports.findAllStates = function (req, res) {
+exports.findAllStates = function (req, res){
     DataResidence.aggregate([ 
         { $match: { pais: req.params.pais } },
         { $group: { _id: "$departamento", departamento: { $max: "$departamento" } } }
@@ -18,12 +18,15 @@ exports.findAllStates = function (req, res) {
         if (err) res.send(500, err.message);
         res.status(200).jsonp(states);
     });
-};
+}
 
-//GET - Retorna un usuario del sistema por un ID especifico
-/*exports.findUserById = function (req, res) {
-    DataResidence.findById(req.params.id, function (err, user) {
-        if (err) return res.send(500, err.message);
-        res.status(200).jsonp(user);
+//GET - Retorna todos las ciudades de un departamento
+exports.findAllCitiesForStates = function (req, res) {
+    DataResidence.find({
+        pais: req.params.pais,
+        departamento: req.params.departamento
+    }, 'ciudad', function (err, cities) {
+        if (err) res.send(500, err.message);
+        res.status(200).jsonp(cities);
     });
-};*/
+}
